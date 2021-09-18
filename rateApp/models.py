025d777ff25fb9fcc37b591fb1c0de2e589356a8
enockabere <maebaenock95@gmail>
2021-09-18 23:03:04 +0300

    create the profile model

--- rateApp/models.py
+++ rateApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    name = models.OneToOneField(User, on_delete=models.CASCADE, blank=True,related_name='profile_user')
+    projects = models.ForeignKey(Project,on_delete=models.CASCADE,related_name='projects')
+    profile_pic = models.ImageField(blank=True)
+    bio = models.TextField(blank=True)
+    contact = models.CharField(max_length=15)
+
+    def __str__(self):
+        return str(self.name.username)
+
