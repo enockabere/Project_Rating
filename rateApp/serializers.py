a9d7016abb570cb2461dd788bd20d5c7bc693ac9
enockabere <maebaenock95@gmail>
2021-09-19 00:23:06 +0300

    create serializers class for my models

--- rateApp/serializers.py
+++ rateApp/serializers.py
@@ -1,2 +1,14 @@
 from rest_framework import serializers
 from .models import Project, Profile, User, Rating
+
+class ProjectSerializer(serializers.ModelSerializer):
+    #create a meta class to define model
+    class Meta:
+        model = Project
+        fields = '_all_'
+    class Meta:
+        model = Profile
+        fields = '_all_'
+    class Meta:
+        model = Rating
+        fields = '_all_'
