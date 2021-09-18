254b34361155ef9d96989da5410557ab96e01789
enockabere <maebaenock95@gmail>
2021-09-19 02:01:00 +0300

    create authentication serializer

--- rateApp/api.py
+++ rateApp/api.py
@@ -5,14 +5,19 @@ from  .models import Project
 #todo viewsets
 class ProjectViewset(viewsets.ModelViewSet):
     #specify queryset to take all model objects
-    queryset = Project.objects.all()
+    
+    # queryset = Project.objects.all()
     
     permission_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticated
     ]
     
     serializer_class = ProjectSerializer
     
     #method to return project of specific user
-    
+    def get_queryset(self):
+        return self.request.user.project_owner.all()
     #submit project based on owner
+    
+    def perform_create(self,serializer):
+        serializer.save(project_owner=self.request.user)
--- rateApp/models.py
+++ rateApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 #assign user model to a variable User
-User = get_user_model
+User = get_user_model()
 
 # Create your models here.
 
