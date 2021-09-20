381eb189c4479b401eaf524ddfe4ed235c815bd7
enockabere <maebaenock95@gmail>
2021-09-20 13:37:38 +0300

    add styles to the registartion template

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,10 +12,14 @@ class NewUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email','password1','password2')
-        
-        def save(self,commit=True):
-            user= super(NewUserForm, self).save(commit=False)
-            user.email = self.cleaned_data['email']
-            if commit:
-                user.save()
-            return user
+    def __init__(self, *args, **kwargs):
+        super(NewUserForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+    def save(self,commit=True):
+        user= super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
