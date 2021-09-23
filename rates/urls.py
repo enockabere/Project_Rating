54a8b1d104e6ca98e64c0d71dcbfe3315ed8f555
enockabere <maebaenock95@gmail>
2021-09-23 15:07:21 +0300

    add functionality to add project functionality to database

--- rates/urls.py
+++ rates/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from  . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('dashboard/', views.dashboard,name="dashboard"),
     path('rates/', views.ratings,name="rates"),
     path('profile/', views.profile,name="profile"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
--- rates/views.py
+++ rates/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from rateApp.models import Project, Profile, Rating
 
 # Create your views here.
 @login_required(login_url='accounts/login/')
 def dashboard(request):
-    return render(request,'main/dashboard.html')
+    project = Project.objects.all()
+    
+    #adding context
+    ctx = {'project':project}
+    return render(request,'main/dashboard.html', ctx)
 @login_required(login_url='accounts/login/')
 def ratings(request):
     return render(request,'main/rate.html')
 @login_required(login_url='accounts/login/')
 def profile(request):
+    
+    if request.method == 'POST':
+        data = request.POST
+        screenshot = request.FILES.get('image')
+        
+        project = Project.objects.create(
+            project_name = data['p-name'],
+            project_owner = request.user,
+            project_link = data ['p-link'],
+            description = data['description'],
+            screenshot = screenshot,         
+        )
+        print(project)
+        return redirect('profile')
     return render(request,'main/profile.html')
